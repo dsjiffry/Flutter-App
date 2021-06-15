0def7af2c0145e526ceadb350fd79f8a3da89e30
sumangala028 <sumangala028@gmail.com>
2021-06-15 22:04:41 +0530

    sources files added

---
+++ lib/IT17160162/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// const Color primaryColor = Color(0xDD000000);
+// const Color secondColor = Color(0xee000000);
+
+const Color primaryColor = Color(0xff0d47a1);
+const Color secondColor = Color(0xff0d47a1);
+
+//0xff68c105
+//const Color secondColor = Color(0xFFFF5252);
+//const Color primaryColor = Color(0xFFB71C1C);
