3e05853241b0b98b50bac985dac470d7e4834b37
Jeyasumangala Rasanayagam <30535710+sumangala028@users.noreply.github.com>
2021-06-16 10:42:21 +0530

    Update register_page.dart

--- lib/IT17160162/register_page.dart
+++ lib/IT17160162/register_page.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:helloworld/home_page.dart';
+import 'home_page.dart';
 import 'constants.dart';
 import 'colors.dart';
 import 'login_screen.dart';
